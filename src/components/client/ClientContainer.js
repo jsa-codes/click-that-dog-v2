5156ff1528bf3728a89be4463ea7f7b16a20c5bb
James Scott <jsa.codes@gmail.com>
2022-09-15 11:51:49 -0500

    Created ClientSearch component and useState to ClientContainer

---
+++ src/components/client/ClientContainer.js
@@ -0,0 +1,10 @@
+import { useState  } from 'react';
+
+export const ClientContainer = () => {
+    
+    const [searchClient, setSearchClient] = useState("")
+
+    return <>
+        <></>
+    </>
+}
