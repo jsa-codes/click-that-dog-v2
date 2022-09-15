563ac69595113b1aa98ce022a0b9691d202e48c5
James Scott <jsa.codes@gmail.com>
2022-09-15 11:54:26 -0500

    Created ClientList and ClientSearch and added those components to ClientContainer

--- src/components/client/ClientContainer.js
+++ src/components/client/ClientContainer.js
@@ -1,10 +1,12 @@
 import { useState  } from 'react';
+import { ClientList } from './ClientList';
 
 export const ClientContainer = () => {
     
     const [searchClient, setSearchClient] = useState("")
 
     return <>
-        <></>
+        <ClientSearch />
+        <ClientList />
     </>
 }
--- src/components/client/ClientList.js
+++ src/components/client/ClientList.js
@@ -1,3 +1,3 @@
 export const ClientList = () => {
-    return 
+    return <></>
 }
